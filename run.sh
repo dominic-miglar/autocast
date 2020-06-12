2a71d5f0c190a419122d0270763d7845288258ed
Dominic Miglar <dominic.miglar@netunix.at>
2020-06-12 17:37:00 +0200

    improved runscript

--- run.sh
+++ run.sh
@@ -3,5 +3,11 @@
 service dbus start
 service avahi-daemon start
 
+until avahi-browse --all --ignore-local --resolve --terminate
+do
+    echo "Waiting for avahi service to be up and running..."
+    sleep 1
+done
+
 npm start
 
